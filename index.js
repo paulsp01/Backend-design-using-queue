66628c94daf8f4a4e0dbaffcf41f3d12717185a7
paulsp01 <paulswarnalee@gmail.com>
2024-06-15 14:41:59 -0400

    commited

---
+++ index.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const jwt=require('jsonwebtoken');
+const bcrypt=require('bcrypt');
+const bodyParser=require('body-parser');
+
+
+  const app=express();
+  app.use(bodyParser.json());
+
+  const users={};
+  app.post('/register',async(req,res)=>{
+    const {username,password}=req.body;
+    const hashedPw=await bcrypt.hash(password);
+    users[username] = hashedPw;
+    res.status(201).send('User registered');
+  });
+
+  app.post('/login',async(req,res)=>{
+    const{username,password}req.body;
+    const hashedPw=users[username];
+    if(hashedPw&& await bcrypt.compare(password,hashedPw)){
+        const token=jwt.sign({username},'secret',{expiresIn:1h});
+        res.json({token});
+    }else{
+        res.status(401).send(Invalid credentials);
+    }
+  });
+
+
+  app.listen(8000,()=>{
+    console.log("Auth server running on port 8000")
+  });
---
+++ queueManager.js
@@ -0,0 +1,27 @@
+const redis=require('redis');
+const client=redis.createClient();
+
+
+function createQueue(clientId){
+    client.lpush;(clientId,'queue created',(err)=>{
+        if(err){
+            console.error('queue creation failed',err);
+
+        }else{
+            console.log("queue created for client",clientId);
+        }
+    });
+}
+
+
+function enqueueRequest(clientId,request){
+    client.rPush(clientId,JSON.stringify(request),(err)=>{
+        if(err){
+            console.error(" Enqueue request failed",err);
+        }else{
+            console.log("Request enqued for client",clientId);
+        }
+    });
+}
+
+module.exports=createQueue
