61e1da3f3016ca7a0b4900a2c48b092230045d57
paulsp01 <paulswarnalee@gmail.com>
2024-06-15 19:03:45 -0400

    commitecommitd

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express=require('express');
 const jwt=require('jsonwebtoken');
 const bcrypt=require('bcrypt');
 const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
 
 
   const app=express();
   app.use(bodyParser.json());
 
-  const users={};
+  mongoose.connect("mongodb://127.0.0.1:27017/authDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // Define User schema and model
+  const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  });
+
+  const User = mongoose.model("User", userSchema);
+
 
 //   app.get('/',(req,res)=>{
 //     res.send('auth server is running');
